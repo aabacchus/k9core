83af9b4735a32c16bf1373866b85a4ee87faae98
qorg11 <qorg@vxempire.xyz>
2020-06-01 22:47:58 +0200

    Errors count to rmdir

--- src/rmdir.c
+++ src/rmdir.c
@@ -3,13 +3,19 @@
 
 int
 main(int argc, char *argv[]) {
+  int errors = 0;
   for(int i = 1; i<argc;i++)
     {
       int fd = rmdir(argv[i]); /* Is it actually a file descriptor? */
       if(fd == -1)
         {
-          fprintf(stderr,"Error removing dir %s\n",argv[i]);
-        }
+	  fprintf(stderr,"Error removing dir %s\n",argv[i]);
+	  errors++;
+	}
+    }
+  if(errors>0)
+    {
+      printf("%i error(s) found\n",errors);
     }
   return 0;
 }
