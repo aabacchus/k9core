602dd4047f372b2fc9d67d10572541a1e7c0efa2
call-cc <callcc@vxempire.xyz>
2020-06-01 15:40:22 -0400

    Fix echo, shorten file size

--- src/echo.c
+++ src/echo.c
@@ -1,10 +1,9 @@
-#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
+  ++argv;
   while(*argv) {
     (void)fputs(*argv, stdout);   // Print argv
     if(*++argv) putchar(' ');     // If multiple things in argv, print a space between them.
