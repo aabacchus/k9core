9077404b75bc80a529814dd23c2be6f84d4c8f65
qorg11 <qorg@vxempire.xyz>
2020-06-01 22:41:59 +0200

    Rewritten rmdir

--- src/rmdir.c
+++ src/rmdir.c
@@ -1,9 +1,15 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int
 main(int argc, char *argv[]) {
-  while(*argv) {
-    rmdir(*argv);
-    *argv++;
-  }
+  for(int i = 1; i<argc;i++)
+    {
+      int fd = rmdir(argv[i]); /* Is it actually a file descriptor? */
+      if(fd == -1)
+        {
+          fprintf(stderr,"Error removing dir %s\n",argv[i]);
+        }
+    }
+  return 0;
 }
