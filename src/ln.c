bdbe212df78bb9d2bf40b87861b50d7dfafba2b0
call-cc <callcc@vxempire.xyz>
2020-06-02 16:19:26 -0400

    Add to TODO and clean up ln

--- src/ln.c
+++ src/ln.c
@@ -12,20 +12,26 @@ main(int argc, char *argv[])
       printf("Usage: ln oldfile newfile\n");
       return 1;
     }
-  
-  int c = getopt(argc, argv, "s");
-  if(c == 's')
+  int opts;
+  int fd;
+  while((opts = getopt(argc, argv, "s:")) != -1)
     {
-      symlink(argv[2],argv[3]);
-      return 0;
+      switch(opts)
+        {
+	case 's': 
+	  symlink(argv[2],argv[3]);
+	  break;
+	case '?':
+	  printf("-%c: Argument not found", optopt);
+	  break;
+	default:
+	  fd = link(argv[1],argv[2]);
+	  if(fd == -1)
+	    {
+	      fprintf(stderr,"Error creating link\n");
+	      return 1;
+	    }
+	  return 0;
+        }
     }
-
-  
-  int fd = link(argv[1],argv[2]);
-  if(fd == -1)
-    {
-      fprintf(stderr,"Error creating link\n");
-      return 1;
-    }
-  return 0;
 }
