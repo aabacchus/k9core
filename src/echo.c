2214494a7793958383a4992ce057a1898bc1c9f2
call-cc <callcc@vxempire.xyz>
2020-06-03 00:32:38 -0400

    Add -n flag to echo

--- src/echo.c
+++ src/echo.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int
 main(int argc, char *argv[])
 {
+  int nflag;
+  if(strcmp(argv[1], "-n") == 0)
+    nflag = 1;
   ++argv;
   while(*argv) {
     (void)fputs(*argv, stdout);   // Print argv
     if(*++argv) putchar(' ');     // If multiple things in argv, print a space between them.
   }
-  putchar('\n');
+  if(nflag) putchar('\n');
   return 0;
 }
