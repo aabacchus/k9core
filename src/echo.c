e4cdd7df499a3e9d508bcf1bcacc4a84f0308469
call-cc <callcc@vxempire.xyz>
2020-06-03 00:33:47 -0400

    slight fix

--- src/echo.c
+++ src/echo.c
@@ -12,6 +12,6 @@ main(int argc, char *argv[])
     (void)fputs(*argv, stdout);   // Print argv
     if(*++argv) putchar(' ');     // If multiple things in argv, print a space between them.
   }
-  if(nflag) putchar('\n');
+  if(!nflag) putchar('\n');
   return 0;
 }
