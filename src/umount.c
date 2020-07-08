43d55a3dfad24a52d629d835d93d65b14a43cb5a
qorg11 <qorg@vxempire.xyz>
2020-07-08 11:09:33 +0200

    -f flag for umount.c (now we use umount2 though)

--- src/umount.c
+++ src/umount.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <sys/mount.h>
 #include <errno.h>
+#include <getopt.h>
 
 int
 main(int argc, char *argv[])
 {
+  int c = getopt(argc, argv, "f");
+  int options = 0; /* No options by default */
+  int destination = 1;
+  if(c == 'f')
+    {
+    options = MNT_FORCE;
+    destination++;
+    }
   if(argc == 1)
     {
       printf("give a directory\n");
       return 1;
     }
-  int fd = umount(argv[1]);
+  int fd = umount2(argv[destination],options);
   if(fd == -1)
     {
       printf("error umounting: %i\n",errno);
