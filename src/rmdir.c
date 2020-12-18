96d5493476e68989fc23992ca608b8899e977bb8
qorg11 <qorg@vxempire.xyz>
2020-12-18 15:05:58 +0100

    Fixed stuff in rmdir

--- src/rmdir.c
+++ src/rmdir.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+
 int
 main(int argc, char *argv[]) {
-  int errors = 0;
-  for(int i = 1; i<argc;i++)
-    {
-      int fd = rmdir(argv[i]); /* Is it actually a file descriptor? */
-      if(fd == -1)
-        {
-	  fprintf(stderr,"Error removing dir %s: %i = %s\n",argv[i],
-		  errno,strerror(errno));
-	  errors++;
+
+	for(int i = 1; i<argc;i++)
+	{
+		if(rmdir(argv[i]))
+		{
+			fprintf(stderr,"rmdir: failed to remove '%s', %s\n",
+				   argv[i],strerror(errno));
+		}
 	}
-    }
-  return 0;
+	return 0;
 }
