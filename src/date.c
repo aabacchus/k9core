1edc320acd39afa98396b9940741ca2de4940061
qorg11 <qorg@vxempire.xyz>
2020-12-26 04:06:34 +0100

    fix indentation in date

--- src/date.c
+++ src/date.c
@@ -7,34 +7,34 @@
 int
 main(int argc,char *argv[])
 {
-     int c;
-     int u = 0;
-     char buffer[256];
-     time_t now;
-     now = time(NULL);
-     char FORMAT[256] = "%c";
-     struct tm *timeinfo;
+	int c;
+	int u = 0;
+	char buffer[256];
+	time_t now;
+	now = time(NULL);
+	char FORMAT[256] = "%c";
+	struct tm *timeinfo;
 
-     while((c = getopt(argc,argv,"u")) != -1)
-     {
-          switch(c)
-          {
-          case 'u': u = 1; break;
-          }
-     }
+	while((c = getopt(argc,argv,"u")) != -1)
+	{
+		switch(c)
+		{
+		case 'u': u = 1; break;
+		}
+	}
 
-     timeinfo = localtime(&now);
+	timeinfo = localtime(&now);
 
-     if(u)
-          timeinfo = gmtime(&now);
+	if(u)
+		timeinfo = gmtime(&now);
 
-     if(argc > optind && argv[optind][0] == '+')
-     {
-          argv[optind]++;
-          strcpy(FORMAT,argv[optind]);
-     }
+	if(argc > optind && argv[optind][0] == '+')
+	{
+		argv[optind]++;
+		strcpy(FORMAT,argv[optind]);
+	}
 
-     strftime(buffer,256,FORMAT,timeinfo);
-     puts(buffer);
-     return 0;
+	strftime(buffer,256,FORMAT,timeinfo);
+	puts(buffer);
+	return 0;
 }
